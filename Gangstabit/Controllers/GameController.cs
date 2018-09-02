ea9da2f98bcd7fbadca920d49ab846795c048a2a
keitaro92 <nicolas.dalmolin@gmail.com>
2018-09-02 12:32:51 +0200

    initial commit

---
+++ Gangstabit/Controllers/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gangstabit.Controllers
+{
+    [Route("api/[controller]")]
+    public class GameController : Controller
+    {
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            value = value.ToHtml();
+            var logPath = $@"C:\Gangstabit\{DateTime.UtcNow.Ticks}.html";
+            var logFile = System.IO.File.Create(logPath);
+            var logWriter = new System.IO.StreamWriter(logFile);
+            logWriter.WriteLine(value);
+            logWriter.Dispose();
+        }
+    }
+}
